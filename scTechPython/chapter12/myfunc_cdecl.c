0aa96256bc947d99d0914d3c3fa29d77387be561
Lee Jaesoo <ljs9643@gmail.com>
2021-09-01 09:37:24 +0900

    Delete scTechPython directory

--- scTechPython/chapter12/myfunc_cdecl.c
+++
@@ -1,4 +0,0 @@
-/* myfunc_cdelc.c */
-int myadd(int x, int y) {
-	return x + y;
-}
--- scTechPython/chapter12/myfunc_stdcall.c
+++
@@ -1,6 +0,0 @@
-/* myfunc_stdcall.c */
-#define EXPORT __declspec(dllexport) __stdcall
-
-EXPORT int myadd(int x, int y) {
-	return x + y;
-}
